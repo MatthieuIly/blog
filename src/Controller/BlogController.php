da2de5bce12dbf4ae4756514b02a16432daaff79
Matthieu ILY <matthieu.ily@docapost.fr>
2021-11-12 16:31:14 +0100

    bug: Add use Annotation Route
    Issue #2

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -14,6 +14,7 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class BlogController
